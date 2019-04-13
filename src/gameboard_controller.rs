d422a5ac4fa39aee69d6fa0a57577e5cce5cd57f
Justin Honegger <orestes.moiran@gmail.com>
2019-04-13 15:42:34 -0700

    working aside from font problem

--- src/gameboard_controller.rs
+++ src/gameboard_controller.rs
@@ -1,4 +1,4 @@
-//! Gamebooard controller
+//! Gameboard controller
 
 use piston::input::GenericEvent;
 
@@ -6,8 +6,11 @@ use crate::Gameboard;
 
 /// handles events for the sudoku game
 pub struct GameboardController {
-    ///stores the gameboard state
+    /// Stores the gameboard state
     pub gameboard: Gameboard,
+    /// Selected cell.
+    pub selected_cell: Option<[usize; 2]>,
+    cursor_pos: [f64; 2],
 }
 
 impl GameboardController {
@@ -15,11 +18,46 @@ impl GameboardController {
     pub fn new(gameboard: Gameboard) -> GameboardController {
         GameboardController {
             gameboard: gameboard,
+            selected_cell: None,
+            cursor_pos: [0.0; 2],
         }
     }
 
     /// handles events
-    pub fn event<E: GenericEvent>(&mut self, e: &E) {
-        
+    pub fn event<E: GenericEvent>(&mut self, pos: [f64; 2], size: f64, e: &E) {
+        use piston::input::{Button, Key, MouseButton};
+
+        if let Some(pos) = e. mouse_cursor_args() {
+            self.cursor_pos = pos;
+        }
+        if let Some(Button::Mouse(MouseButton::Left)) = e.press_args() {
+            //find coordinates relative to upper left corner
+            let x = self.cursor_pos[0] - pos[0];
+            let y = self.cursor_pos[1] - pos[1];
+            //check that coordinates are isnide the board.
+            if x >= 0.0 && x < size && y >= 0.0 && y < size {
+                //computer the cell position
+                let cell_x = (x / size * 9.0) as usize;
+                let cell_y = (y / size * 9.0) as usize;
+                self.selected_cell = Some([cell_x, cell_y]);
+            }
+        }
+        if let Some(Button::Keyboard(key)) = e.press_args() {
+            if let Some(ind) = self.selected_cell {
+                //set the cell value
+                match key {
+                    Key::D1 => self.gameboard.set(ind, 1),
+                    Key::D2 => self.gameboard.set(ind, 2),
+                    Key::D3 => self.gameboard.set(ind, 3),
+                    Key::D4 => self.gameboard.set(ind, 4),
+                    Key::D5 => self.gameboard.set(ind, 5),
+                    Key::D6 => self.gameboard.set(ind, 6),
+                    Key::D7 => self.gameboard.set(ind, 7),
+                    Key::D8 => self.gameboard.set(ind, 8),
+                    Key::D9 => self.gameboard.set(ind, 9),
+                    _ => {}
+                }
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ extern crate glutin_window;
 extern crate graphics;
 extern crate opengl_graphics;
 
+use piston::window::WindowSettings;
+use piston::event_loop::{Events, EventLoop, EventSettings};
+use piston::input::RenderEvent;
+use glutin_window::GlutinWindow;
+use opengl_graphics::{OpenGL, Filter, GlGraphics, GlyphCache, TextureSettings};
+
 pub use crate::gameboard::Gameboard;
 pub use crate::gameboard_controller::GameboardController;
 pub use crate::gameboard_view::{GameboardView, GameboardViewSettings};
@@ -15,13 +21,10 @@ mod gameboard;
 mod gameboard_controller;
 mod gameboard_view;
 
-use piston::window::WindowSettings;
-use piston::event_loop::{Events, EventLoop, EventSettings};
-use glutin_window::GlutinWindow;
-use opengl_graphics::{OpenGL, GlGraphics};
-
 fn main() {
-    let settings = WindowSettings::new("Sudoku", [512; 2])
+    let opengl = OpenGL::V3_2;
+    let settings = WindowSettings::new("rustdoku", [512; 2])
+        .opengl(opengl)
         .exit_on_esc(true);
     let mut window: GlutinWindow = settings.build()
         .expect("Could not create window");
@@ -33,18 +36,22 @@ fn main() {
     let mut gameboard_controller = GameboardController::new(gameboard);
     let gameboard_view_settings = GameboardViewSettings::new();
     let gameboard_view = GameboardView::new(gameboard_view_settings);
+
+    let texture_settings = TextureSettings::new().filter(Filter::Nearest);
+    let ref mut glyphs = GlyphCache::new("assets/FiraSans-Regular.ttf", (), texture_settings)
+        .expect("Could not load font");
     
     while let Some(e) = events.next(&mut window) {
-        gameboard_controller.events(&e);
+        gameboard_controller.event( gameboard_view.settings.position,
+                                    gameboard_view.settings.size,
+                                    &e);
         if let Some(args) = e.render_args() {
             gl.draw(args.viewport(), |c, g| {
                 use graphics::{clear};
 
                 clear([1.0; 4], g);
-                gameboard_view.draw(&gameboard_controller, &c, g);
+                gameboard_view.draw(&gameboard_controller, glyphs, &c, g);
             });
         }
     }
-
-    println!("{}", settings.get_exit_on_esc());
 }
